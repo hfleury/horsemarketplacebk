99e43beb1c5123d74d8e5130a7a05a4f260100bf
henriqueoni <henrique@oniweb.com.br>
2025-01-15 20:58:10 +0100

    Add signature of a method to get the logger from the context

--- config/logging.go
+++ config/logging.go
@@ -5,4 +5,5 @@ import "context"
 type Logging interface {
 	Log(ctx context.Context, level LogLevel, msg string, fields map[string]any)
 	WithTrace(ctx context.Context, traceID string) context.Context
+	GetLoggerFromContext(c context.Context) Logging
 }
