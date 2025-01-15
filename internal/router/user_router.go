a8fb0b7430cbf35987dae78e3a75450f5a4f8a06
henriqueoni <henrique@oniweb.com.br>
2025-01-15 21:01:41 +0100

    Add the logger

--- internal/router/user_router.go
+++ internal/router/user_router.go
@@ -2,11 +2,12 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/hfleury/horsemarketplacebk/config"
 	"github.com/hfleury/horsemarketplacebk/internal/auth/handlers"
 )
 
-func registerUserRoutes(router *gin.Engine) {
-	userHandler := handlers.NewUserHandler()
+func registerUserRoutes(router *gin.Engine, logger config.Logging) {
+	userHandler := handlers.NewUserHandler(logger)
 
 	authRoutes := router.Group("/auth")
 	{
