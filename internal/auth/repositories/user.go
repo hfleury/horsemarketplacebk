2ef1f8e94fe1dfaf6d3241c2ee76ba7c958e4202
henriqueoni <henrique@oniweb.com.br>
2025-01-17 15:21:49 +0100

    Add the Insert method signature to the interface

--- internal/auth/repositories/user.go
+++ internal/auth/repositories/user.go
@@ -1,8 +1,13 @@
 package repositories
 
-import "context"
+import (
+	"context"
+
+	"github.com/hfleury/horsemarketplacebk/internal/auth/models"
+)
 
 type UserRepository interface {
 	IsUsernameTaken(ctx context.Context, username string) (bool, error)
 	IsEmailTaken(ctx context.Context, email string) (bool, error)
+	Insert(ctx context.Context, user *models.User) (*models.User, error)
 }
